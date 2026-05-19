c8cd88f0331308b9c5286ac89ce2f05556c46c51
Vuthuhuong <huonginht123@gmail.com>
2026-05-19 18:40:40 +0700

    thêm

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller 
 {
-    // 1. Hiển thị form đăng nhập 
+    // 1. Hiển thị form đăng nhập trung anh very 
     public function showLogin() 
     {
         return view('auth.login'); 
